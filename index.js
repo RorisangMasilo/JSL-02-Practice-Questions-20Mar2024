ec29f88151f8e703514f92460b4cbd7ed3ba6377
Rorisang Masilo <152070569+RorisangMasilo@users.noreply.github.com>
2024-03-21 17:29:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,25 @@ function toggleClass() {
   let element = document.getElementById("myHeading");
   element.classList.toggle("highlight");
 }
+
+// How can the style/class of an element be changed using DOM Manipulation? Write a function for this
+
+// Function to change the style of an element
+/* let headerNode = document.getElementById("myHeading");
+
+function changeStyle() {
+  headerNode.style.color = "purple";
+  headerNode.style.fontSize = "48px";
+}
+
+// Function to change the class of an element
+function changeClass() {
+  headerNode.classList.add("header");
+  headerNode.classList.remove("highlight");
+}
+
+function toggleClass() {
+  headerNode.classList.toggle("highlight");
+}
+
+Now you can adjust the JS script to declare the variable once 🙂
